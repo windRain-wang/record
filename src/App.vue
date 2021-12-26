924a0b09816a144e6713de2b9c9b0e6c9c24b14d
王明恒 <forkingw@aliyun.com>
2021-12-27 01:38:15 +0800

    创建 项目1

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold underline text-emerald-600">Hello world!</h1>
+</template>
+
+<style lang="pcss"></style>
